d16a5bb3c3466ae2906b729f1e6f00678f6f5835
bonophb <ejstkah@gmail.com>
2015-04-03 13:56:46 +0900

    Gunshot

---
+++ Maze Game/Assets/Script/Gunshot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gunshot : MonoBehaviour {
+    public GameObject shotpoint;
+    public GameObject bullet;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetMouseButtonDown(0))
+        {
+            GameObject temp = (GameObject)Instantiate(bullet, shotpoint.transform.position, Quaternion.identity);
+            temp.GetComponent<Rigidbody>().AddForce(shotpoint.transform.forward * 300f);
+        }
+	}
+}
---
+++ Maze Game/Assets/Script/bullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        if(transform.position.y<0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        Destroy(gameObject);
+    }
+}
